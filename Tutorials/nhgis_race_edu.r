401a95a57c2532eb49f8df668baa0a28c1969c96
grossi63 <rossig@uw.edu>
2021-07-31 20:00:55 -0700

    through AIAN

--- Tutorials/nhgis_race_edu.r
+++ Tutorials/nhgis_race_edu.r
@@ -19,19 +19,19 @@ var_df$concept <- gsub(paste0("SEX BY EDUCATIONAL ",
                               "ATTAINMENT FOR THE ", 
                               "POPULATION 25 YEARS ", 
                               "AND OVER \\("), "", 
-                              var_df$concept[grepl("C15002", var_df$name)])
+                       var_df$concept[grepl("C15002", var_df$name)])
 ###########################################
 # -- load King County tract-level data -- #
 ###########################################
 ##PART 1
 ## create white only tract-level dataset for King county
 tract_edu_white <- get_acs("tract",
-                     table = "C15002A",
-                     year = 2018,
-                     state = "WA",
-                     county = "King",
-                     geometry = TRUE, 
-                     survey = "acs5")
+                           table = "C15002A",
+                           year = 2018,
+                           state = "WA",
+                           county = "King",
+                           geometry = TRUE, 
+                           survey = "acs5")
 
 table_C15002A <- var_df %>%
   filter(str_starts(name, "C15002A")) %>%
@@ -400,41 +400,179 @@ Black_college_grad_CoV_plot <- Black_college_grad %>%
 ####################### PART 3
 ###create data set and plot for American Indian/Alaska Native edu attainment
 tract_edu_AIAN <- get_acs("tract",
-                           table = "C15002C",
-                           year = 2018,
-                           state = "WA",
-                           county = "King",
-                           geometry = TRUE, 
-                           survey = "acs5")
+                          table = "C15002C",
+                          year = 2018,
+                          state = "WA",
+                          county = "King",
+                          geometry = TRUE, 
+                          survey = "acs5")
 
 table_C15002C <- var_df %>%
   filter(str_starts(name, "C15002C")) %>%
   mutate(short_label = str_split(label, "!!"), 
-         short_label = map_chr(short_label, tail, 1)) %>%
+         short_label = map_chr(short_label, tail, 1)) %>% 
   dplyr::select(name, short_label)
 
-tract_edu_AIAN_lab <- tract_edu_AIAN %>%
-  left_join(table_C15002C, by = c("variable" = "name"))
+#create my own indicators
+table_C15002C$less_than_hs <- ifelse(table_C15002C$short_label == "Less than high school diploma", 1, 0)
+table_C15002C$hs_grad <- ifelse(table_C15002C$short_label == "High school graduate (includes equivalency)", 1, 0)
+table_C15002C$some_college <- ifelse(table_C15002C$short_label == "Some college or associate's degree", 1, 0)
+table_C15002C$college_grad <- ifelse(table_C15002C$short_label == "Bachelor's degree or higher", 1, 0)
 
-# We discard the total type for now
-tract_edu_AIAN <- tract_edu_AIAN_lab %>%
-  filter(!(short_label %in% c("Total", "Female", "Male"))) %>%
-  group_by(GEOID, short_label) %>%
+tract_edu_AIAN_lab <- tract_edu_AIAN %>%
+  left_join(table_C15002C, by = c("variable" = "name")) 
+
+######## Part 3A - AIAN less than HS attainment
+### create AIAN less than HS dataset 
+AIAN_less_than_hs <- tract_edu_AIAN_lab %>%
+  filter(less_than_hs == 1) %>%
+  group_by(GEOID, less_than_hs) %>% 
   summarize(
     estimate = sum(estimate),
     moe = sum(moe)
   ) %>%
   data.table()
 
-tract_edu_AIAN <- tract_edu_AIAN[, summary_est := sum(estimate), by = 'GEOID']
-tract_edu_AIAN <- tract_edu_AIAN[, prop := estimate/summary_est]
+## calculate SE and CoV 
+AIAN_less_than_hs<- AIAN_less_than_hs %>% 
+  mutate(SE = moe/qnorm(.95)) %>% 
+  mutate(CoV = SE/estimate)
 
-##create plot for American Indian/Alaska Native educational attainment
-tract_edu_AIAN %>%
+##create AIAN less than HS educational attainment plot for estimate only - how do i get 1 to go away?
+AIAN_less_than_hs_estimate_plot <- AIAN_less_than_hs %>%
   st_as_sf() %>%
   ggplot() +
-  geom_sf(aes(fill = prop), size = .25) +
-  facet_wrap(vars(short_label)) +
+  geom_sf(aes(fill = estimate)) +
+  labs(title = "Less than High School Education, American Indian/Alaska Native",
+       subtitle = "Using Estimate Value")+
+  facet_wrap(vars(less_than_hs)) +
+  scale_fill_viridis_c() + 
+  theme_void()
+
+##create AIAN less than HS educational attainment plot for CoV - fix labels
+AIAN_less_than_hs_CoV_plot <- AIAN_less_than_hs %>% 
+  st_as_sf() %>%
+  ggplot() +
+  geom_sf(aes(fill = CoV)) +
+  labs(title = "Less than High School Education, American Indian/Alaska Native",
+       subtitle = "Coefficient of Variance",
+       fill = "Coefficient of Variance") +
+  facet_wrap(vars(less_than_hs)) +
+  scale_fill_viridis_c() + 
+  theme_void()
+
+#####Part 3B - AIAN high school grad/equivalent
+AIAN_hs <- tract_edu_AIAN_lab %>%
+  filter(hs_grad == 1) %>%
+  group_by(GEOID, hs_grad) %>% 
+  summarize(
+    estimate = sum(estimate),
+    moe = sum(moe)
+  ) %>%
+  data.table()
+
+## calculate SE and CoV 
+AIAN_hs<- AIAN_hs %>% 
+  mutate(SE = moe/qnorm(.95)) %>% 
+  mutate(CoV = SE/estimate)
+
+##create AIAN HS grad/equiv educational attainment plot for estimate only - how do i get 1 to go away?
+AIAN_hs_estimate_plot <- AIAN_hs %>%
+  st_as_sf() %>%
+  ggplot() +
+  geom_sf(aes(fill = estimate)) +
+  labs(title = "High School Graduate or Equivalent Education, American Indian/Alaska Native",
+       subtitle = "Using Estimate Value")+
+  facet_wrap(vars(hs_grad)) +
+  scale_fill_viridis_c() + 
+  theme_void()
+
+##create AIAN less than HS educational attainment plot for CoV - fix labels
+AIAN_hs_CoV_plot <- AIAN_hs %>% 
+  st_as_sf() %>%
+  ggplot() +
+  geom_sf(aes(fill = CoV)) +
+  labs(title = "High School Graduate or Equivalent Education, American Indian/Alaska Native",
+       subtitle = "Coefficient of Variance",
+       fill = "Coefficient of Variance") +
+  facet_wrap(vars(hs_grad)) +
+  scale_fill_viridis_c() + 
+  theme_void()
+
+### part 3C - AIAN some college/AA attainment 
+AIAN_SC_AA <- tract_edu_AIAN_lab %>%
+  filter(some_college == 1) %>%
+  group_by(GEOID, some_college) %>% 
+  summarize(
+    estimate = sum(estimate),
+    moe = sum(moe)
+  ) %>%
+  data.table()
+
+## calculate SE and CoV 
+AIAN_SC_AA<- AIAN_SC_AA %>% 
+  mutate(SE = moe/qnorm(.95)) %>% 
+  mutate(CoV = SE/estimate)
+
+##create AIAN some college/AA plot for estimate only 
+AIAN_SC_AA_estimate_plot <- AIAN_SC_AA %>%
+  st_as_sf() %>%
+  ggplot() +
+  geom_sf(aes(fill = estimate)) +
+  labs(title = "Some College or Associate's Degree, American Indian/Alaska Native",
+       subtitle = "Using Estimate Value")+
+  facet_wrap(vars(some_college)) +
+  scale_fill_viridis_c() + 
+  theme_void()
+
+##create AIAN some college/AA educational attainment plot for CoV - fix labels
+AIAN_SC_AA_CoV_plot <- AIAN_SC_AA %>% 
+  st_as_sf() %>%
+  ggplot() +
+  geom_sf(aes(fill = CoV)) +
+  labs(title = "Some College or Associate's Degree, American Indian/Alaska Native",
+       subtitle = "Coefficient of Variance",
+       fill = "Coefficient of Variance") +
+  facet_wrap(vars(some_college)) +
+  scale_fill_viridis_c() + 
+  theme_void()
+
+####Part 2D: AIAN Bachelor's or higher degree attainment
+
+AIAN_college_grad <- tract_edu_AIAN_lab %>%
+  filter(college_grad == 1) %>%
+  group_by(GEOID, college_grad) %>% 
+  summarize(
+    estimate = sum(estimate),
+    moe = sum(moe)
+  ) %>%
+  data.table()
+
+## calculate SE and CoV 
+AIAN_college_grad<- AIAN_college_grad %>% 
+  mutate(SE = moe/qnorm(.95)) %>% 
+  mutate(CoV = SE/estimate)
+
+##create AIAN college grad plot for estimate only - how do i get 1 to go away?
+AIAN_college_grad_estimate_plot <- AIAN_college_grad %>%
+  st_as_sf() %>%
+  ggplot() +
+  geom_sf(aes(fill = estimate)) +
+  labs(title = "Bachelor's Degree or Higher, American Indian/Alaska Native",
+       subtitle = "Using Estimate Value")+
+  facet_wrap(vars(college_grad)) +
+  scale_fill_viridis_c() + 
+  theme_void()
+
+##create AIAN college grad educational attainment plot for CoV - fix labels
+AIAN_college_grad_CoV_plot <- AIAN_college_grad %>% 
+  st_as_sf() %>%
+  ggplot() +
+  geom_sf(aes(fill = CoV)) +
+  labs(title = "Bachelor's Degree or Higher, American Indian/Alaska Native",
+       subtitle = "Coefficient of Variance",
+       fill = "Coefficient of Variance") +
+  facet_wrap(vars(college_grad)) +
   scale_fill_viridis_c() + 
   theme_void()
 
@@ -477,4 +615,3 @@ tract_edu_Asian %>%
   facet_wrap(vars(short_label)) +
   scale_fill_viridis_c() + 
   theme_void()
-
