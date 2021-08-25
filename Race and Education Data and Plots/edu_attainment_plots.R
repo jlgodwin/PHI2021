1e75ee54f8526ef5291c9cf317add7c3d6904cd6
grossi63 <72241333+grossi63@users.noreply.github.com>
2021-08-24 18:11:26 -0700

    horrid file

--- Race and Education Data and Plots/edu_attainment_plots.R
+++ Race and Education Data and Plots/edu_attainment_plots.R
@@ -14,8 +14,34 @@ library(classInt)
 library(scales) 
 library(tidyr)
 library(jsonlite)
+library(rjson)
+library(geojsonsf)
 
 edu_data <- read.csv("edu_attainment_data.csv")
+horrid <- fromJSON(file = "kc_tract.json")
+##tracts <- geojson_sf(tracts)
+
+
+feature_test <- list()
+featuredf_test <- list()
+feature_geo_test <- list()
+for(feature.id in 1:length(horrid$features)){
+  tmp_geometry <- horrid$features[[feature.id]]$geometry
+  tmp_coords <- tmp_geometry$coordinates
+  feature_test[[feature.id]] <- lapply(unlist(tmp_coords),
+              function(coords_elem){ coords_elem[[1]]
+               
+              } )
+  
+featuredf_test[[feature.id]] <- 
+  data.frame(long = unlist(feature_test[[feature.id]][seq(1,
+                                                          length(feature_test[[feature.id]]),2)]),
+             lat = unlist(feature_test[[feature.id]][seq(2,
+                                                         length(feature_test[[feature.id]]),2)]))  
+feature_geo_test[[feature.id]] <- df_geojson(featuredf_test[[feature.id]], 
+                                             lon = "long", lat = "lat") %>% 
+  geojson_sf()
+}
 
 years <- c(2009, 2014, 2019)
 less_than_hs_white_estimate <- list()
